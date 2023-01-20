77a25e0b76e93524b38e9bdcd0333fb3bc2bbed2
yuladp1 <yuladp1@gmail.com>
2023-01-21 00:39:49 +0100

    init commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-9 ma-4">
+    <h1>Something interesting about...</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores eius
+      doloribus unde enim sed minus asperiores, officiis deleniti vel ut nam
+      iure accusamus fugiat quae nihil. Doloribus officia a sint?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, provident
+      ipsum quos totam aspernatur dignissimos distinctio magni illum sequi
+      voluptate numquam praesentium, dolor dolorum labore qui, similique
+      asperiores! Accusantium, earum.
+    </p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="pa-6">
+    <h1>Todo List</h1>
+    <v-list subheader flat>
+      <v-list-item>
+        <template v-slot:default="{ active }">
+          <v-list-item-action>
+            <v-checkbox :input-value="active" color="primary"></v-checkbox>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Notifications</v-list-item-title>
+          </v-list-item-content>
+        </template>
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  name: "Home-view",
 };
 </script>
